b8c41efdefad651115d96d77d916e7afa8accfce
farhanlibra1984 <145924162+farhanlibra1984@users.noreply.github.com>
2023-12-18 10:10:07 -0800

    Add files via upload

---
+++ api.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const app = express();
+
+dotenv.config()
+app.use(cors());
+
+app.get('/api/test', (req, res)=>{
+    res.json("hello"+Date.now());
+})
+
+app.get('/api/test2', (req, res)=>{
+    res.json("hello farhan"+Date.now());
+})
+
+
+if(process.env.API_PORT){
+    app.listen(process.env.API_PORT);
+}
+
+module.exports=app;
