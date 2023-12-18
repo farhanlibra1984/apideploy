4a45e9765cd148c164a10669ae8ef537da9fe212
farhanlibra1984 <farhanlibra1984@outlook.com>
2023-12-18 10:13:09 -0800

    2

--- api/api.js
+++ api/api.js
@@ -10,10 +10,6 @@ app.get('/api/test', (req, res)=>{
     res.json("hello"+Date.now());
 })
 
-app.get('/api/test2', (req, res)=>{
-    res.json("hello farhan"+Date.now());
-})
-
 
 if(process.env.API_PORT){
     app.listen(process.env.API_PORT);
